abc7cf9e935328039a4cdba19855e33a9e0c6f1e
Mohamed ED-Derkaoui <med.derkaoui99@gmail.com>
2023-06-20 14:00:45 -0700

    update code in the file 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 
+/**
+*main - function prints text as output
+*
+*Return:return 0
+*/
 int main(void)
 {
-putchar('_');
-putchar('p');
-putchar('u');
-putchar('t');
-putchar('c');
-putchar('h');
-putchar('a');
-putchar('r');
-return (0);
+	char school[10] = "_putchar";
+
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(school[i]);
+	}
+	_putchar(10);
+	return (0);
 }
