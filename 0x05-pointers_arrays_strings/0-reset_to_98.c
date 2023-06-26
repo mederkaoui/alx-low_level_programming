478c56c091e61eeed1401184d50ed6e70e4c0925
Mohamed ED-Derkaoui <med.derkaoui99@gmail.com>
2023-06-26 15:45:36 -0700

    Writed a function that swaps the values of two integers, into the file 1-swap.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,12 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
- * reset_to_98 - a function that takes a pointer to an int as parameter
- * and updates the value it points to to 98.
- * @n: An integer input pointer
- * Return: Nothing.
- */
+*reset_to_98 - reset value of an integer to 98
+*@n: parameter targeted
+*Return : nothing
+*/
+
 void reset_to_98(int *n)
 {
-	*n = 98;
+*n = 98;
 }
 
